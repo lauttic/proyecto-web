af0a3d30dc73f2964ba0e0c3058a44edb2d6c165
Lautaro <lcorceiro1997@gmail.com>
2025-04-30 13:04:46 -0300

    images cards

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,10 @@
 const menuContainer = document.getElementById("menu-container");
 
-fetch('../assets/productos.json')
+fetch('/assets/productos.json')
   .then(response => response.json())
   .then(productos => {
-    // Ahora sí tenemos los datos, renderizamos el menú
     renderMenu(productos);
+    console.log(productos)
   })
   .catch(error => {
     console.error("Error al cargar el menú:", error);
@@ -13,8 +13,10 @@ fetch('../assets/productos.json')
 function renderMenu(menuData) {
   menuData.menu.forEach(categoria => {
     categoria.items.forEach(item => {
+        console.log(item)
       const card = document.createElement("div");
       card.className = "menu-card";
+
 
       card.innerHTML = `
         <img src="${item.imagen}" alt="${item.nombre}">
@@ -28,3 +30,29 @@ function renderMenu(menuData) {
     });
   });
 }
+
+// ---- NAVEGACIÓN DINÁMICA ----
+const navLinks = document.querySelectorAll(".nav-menu a");
+const sections = document.querySelectorAll("main section");
+
+function mostrarSeccion(id) {
+  sections.forEach(section => {
+    section.style.display = section.id === id ? "block" : "none";
+  });
+}
+
+// Ocultamos todo menos "home" al cargar
+mostrarSeccion("home");
+
+// Asignamos los eventos a los links
+navLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    const sectionId = link.getAttribute("data-section");
+    mostrarSeccion(sectionId);
+
+    // (opcional) marcar como activo
+    navLinks.forEach(l => l.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
